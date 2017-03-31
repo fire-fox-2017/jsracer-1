a6c5abafa83fb90bd44909cb2ca3dc1ac80054ed
ambo-dalle <ambodalle.st@gmail.com>
2017-03-31 18:12:09 +0700

    JS Racer

--- js_racer.js
+++ js_racer.js
@@ -3,18 +3,63 @@
 import Dice from "./dice.js"
 
 class JSRacer {
-  constructor(players, length, sides) {
-
+  constructor(players, length) {
+    this.players = players //[a,b,c]
+    this.posisi = this.count_player(players) //[0,0,0]
+    this.length = length
+    this.winner = ''
+    this.dice = new Dice()
+    this.status = true
+    this.begin = true
   }
+  count_player(players){
+    let start = []
+    for (let i=0; i<players.length; i++){
+      start.push(0)
+    }
+    console.log(start);
+    return start
+  }
+
   print_board() {
-
+    while(this.status == true){
+      if (this.begin){
+        for(let i=0; i<this.players.length; i++){
+          this.print_line(this.players[i],this.posisi[i])
+        }
+        this.begin = false
+      } else {
+        for (let i=0; i<this.players.length; i++){
+          if(this.status){
+            this.posisi[i] += this.dice.roll()
+            // console.log(this.posisi[i]);
+          }
+          if(this.posisi[i] >= this.length-1){
+            this.posisi[i] = this.length - 1
+            this.status = false
+            this.winner = this.players[i]
+          }
+          this.print_line(this.players[i],this.posisi[i])
+        }
+      }
+    }
+    if (!this.status){
+      console.log(`The Winner: ${this.winner}`);
+    }
   }
-  print_line(player, pos) {
 
+  print_line(players, pos) {
+    let arr = []
+    for (let i=0; i<this.length; i++){
+      if(i===pos){
+        arr.push(players)
+      } else {
+        arr.push(' ')
+      }
+    }
+    console.log(arr.join('|'));
   }
-  advanced_player(player) {
 
-  }
   finished() {
 
   }
@@ -24,6 +69,14 @@ class JSRacer {
   reset_board() {
     console.log("\x1B[2J")
   }
+
 }
+
+
+let play = new JSRacer (['A','B','C'],15)
+console.log(play.print_line(play.players, play.start));
+
+play.print_board();
+ console.log(play.players);
 
 export default JSRacer
